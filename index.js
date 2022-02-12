c3752f03d06c40e5a485cc84cba94d53b2bb5292
Pablo Tack <tackpablo@gmail.com>
2022-02-11 18:19:43 -0800

    sum all numbers logic completed

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  // result variable
+  let sum = 0;
+  // for each element of the array
+  array.forEach((el) => {
+    // check if there are nested arrays
+    if (Array.isArray(el)) {
+      // if true, run that array again through the function to loop through the nested arrays (can reach any level) while adding sum of that array
+      sum += sumItems(el);
+    } else {
+      // add the element to result variable as they are not nested
+      sum += el;
+    }
+  });
+  return sum;
 }
 
 module.exports = sumItems;
